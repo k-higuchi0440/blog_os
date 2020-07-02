27d4caff2ab9e76264462ab17b2e0e9576432bb7
k_higuchi <koyohiguchi7788@gmail.com>
2020-07-02 21:48:55 +0900

    2 Minimum Rust Kernel

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-#![no_std]
-#![no_main]
+#![no_std] // dont link the Rust standard libeary
+#![no_main] // disable Rust-level entry points
 
-#[no_mangle]
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle] // dont mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
